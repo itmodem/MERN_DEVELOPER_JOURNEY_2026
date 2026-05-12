30bf740778857b5a27e7dfa92efd5f3f1225492e
itmodem <info@itmodem.com>
2026-05-12 07:09:01 +0530

    Day 1 Logic Building

---
+++ 10_DAILY_LOGS/D1/day1.js
@@ -0,0 +1,33 @@
+// let name = "IT Modem";
+// let age = "29";
+// let isAdmission = "true";
+
+// console.log(name);
+// console.log(age);
+// console.log(isAdmission);
+
+// let user = {
+//     name = "IT Modem",
+//     age = 23, 
+// };
+
+// let skills = ["React", "JS", "Node"];
+
+// function name () {
+
+// }
+
+// function name(params) {
+    
+// }
+
+// function test () {
+//     let x = 10;
+// };
+
+if(true){
+let a = 20;
+var b = 10;
+}
+
+console.log(a);
